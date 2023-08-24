061450b612c8effdd336753396c2e0c29488d70e
Abd-Elrahman Nawar <89775361+Abd-Elrhman20@users.noreply.github.com>
2023-08-24 16:03:55 +0300

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Abd-Elrhman20",
-  resume: "https://drive.google.com/file/d/1E8JkpKspgpJ058V9yMXmCAUmYvf3Cln3/view?usp=sharing",
+  resume: "https://drive.google.com/file/d/1t-eWPYqUGGLXHvHZMmXp2vT3hmSpO7hn/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/abd-elrahman-nawar-827110217/",
   // twitter: "https://twitter.com/RishavChanda",
   insta: "https://www.instagram.com/abd_elrhmannawar/",
